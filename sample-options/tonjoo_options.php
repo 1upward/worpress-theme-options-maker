782e3d83fb040fe399ce39e78e3ff3f9c69acd2c
Lafif Astahdziq <lafif@astahdziq.in>
2014-06-06 19:28:39 +0700

    add list available config

--- sample-options/tonjoo_options.php
+++ sample-options/tonjoo_options.php
@@ -2,8 +2,20 @@
 
 function tonjoo_tom_config($config) {
 
-	$config['mode'] = 'full';
-	$config['menu_title'] = 'Oooops';
+#	$config['mode'] 			= 'full';
+#   $config['page_title'] 		= '';
+#   $config['page_desc'] 		= '';
+#	$config['menu_title'] 		= 'Test';
+#	$config['capability'] 		= '';
+#	$config['menu_slug'] 		= '';
+#   $config['icon_url']			= '';  /* Please refer to http://melchoyce.github.io/dashicons/ *
+#   $config['position'] 		= '';
+#   $config['parent_slug'] 		= '';
+#   $config['sub_page_title'] 	= '';
+#   $config['sub_page_desc'] 	= '';
+#	$config['sub_menu_title'] 	= '';
+#	$config['sub_capability'] 	= '';
+#	$config['sub_menu_slug'] 	= '';
 
 	return $config;
 }
